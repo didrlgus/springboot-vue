3d64d2b67d1e9d433c12480e837cfe2a9dbe2cdf
양기현 <rlgusdid@naver.com>
2019-12-24 22:27:30 +0900

    Vue를 이용하여 Todo List 기능을 수행하는 웹 사이트 개발 (기본적인 CRUD 기능 수행)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,68 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <TodoHeader></TodoHeader>
+    <TodoInput v-on:addTodo="addTodo"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" @removeTodo="removeTodo"></TodoList>
+    <TodoFooter v-on:removeAll="clearAll"></TodoFooter>
   </div>
 </template>
 
 <script>
+import TodoHeader from './components/TodoHeader'
+import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
+import TodoFooter from './components/TodoFooter'
+
 export default {
-  name: 'App'
+  data () {
+    return {
+      todoItems: []
+    }
+  },
+  created () {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i))
+      }
+    }
+  },
+  methods: {
+    addTodo (todoItem) {
+      // 로컬 스토리지에 데이터를 추가하는 로직
+      localStorage.setItem(todoItem, todoItem)
+      this.todoItems.push(todoItem)
+    },
+    clearAll () {
+      localStorage.clear()
+      this.todoItems = []
+    },
+    removeTodo (todoItem, index) {
+      localStorage.removeItem(todoItem)
+      this.todoItems.splice(index, 1)
+    }
+  },
+  components: {
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    text-align:center;
+    background-color: #F6F6F8;
+  }
+  input {
+    border-style: groove;
+    width: 200px;
+  }
+  button {
+    border-style: groove;
+  }
+  .shadow {
+    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+  }
 </style>
